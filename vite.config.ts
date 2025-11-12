122b62a23ea41e716e2c44addac1ac76e19dd0f0
rznn7 <jest1gabriel@gmail.com>
2025-11-12 23:38:46 +0100

    gh-page deployment

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/http-req-diff/',
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
